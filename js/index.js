15477c6ddaf109dd9f649bc0b2ec8e63b0013cb2
JacquesFFourie <125289955+JacquesFFourie@users.noreply.github.com>
2023-05-29 07:19:22 +0200

    Data Collection Implemented
    Data from forms gathered and stored in an array

---
+++ js/index.js
@@ -0,0 +1,30 @@
+let userSignup = [];
+
+addUser = () => {
+    let userNameSurname = document.getElementById("userNameSurname").value;
+    let userNumber = document.getElementById("userNumber").value;
+    let userEmail = document.getElementById("userEmail").value;
+    let userSubject = document.getElementById("userSubject").value;
+    let userPassword = document.getElementById("userPassword").value;
+
+    let userBoxEmail = document.getElementById("userBoxEmail").checked;
+    console.log(userBoxEmail)
+
+
+    userSignup.push({
+        userName: userNameSurname,
+        userPhoneNumber: userNumber,
+        userEmail: userEmail,
+        userSubject: userSubject,
+        userPassword: userPassword
+
+    });
+    
+    if(userBoxEmail == true){
+        alert("Welcome " + userNameSurname + "! Thank you for your interest in " + userSubject + "! We will contact you via email provided: " + userEmail + ", or alternatively on the phone number: " + userNumber + ".")
+    } else{
+        alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested Name & Surname!  If you want to sign up, you better CHECK that newsletter box! ")
+    }
+    document.getElementById("studentForm").reset();
+}
+
