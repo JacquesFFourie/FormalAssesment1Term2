a2a0718bf4836ef7fa1405901cf96f5344ff3861
JacquesFFourie <125289955+JacquesFFourie@users.noreply.github.com>
2023-05-29 07:24:34 +0200

    Update to Alert
    Mis naming in alert fixed

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ addUser = () => {
     if(userBoxEmail == true){
         alert("Welcome " + userNameSurname + "! Thank you for your interest in " + userSubject + "! We will contact you via email provided: " + userEmail + ", or alternatively on the phone number: " + userNumber + ".")
     } else{
-        alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested Name & Surname!  If you want to sign up, you better CHECK that newsletter box! ")
+        alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested " + userNameSurname +"  If you want to sign up, you better CHECK that newsletter box! ")
     }
     document.getElementById("studentForm").reset();
 }
